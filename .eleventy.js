86e53f70a0bdedf95f63f181c70c63bb9c7c9bb7
soler marjorie <soler.marjorie81@gmail.com>
2022-08-22 15:22:34 +0200

    Création de la base et du header, footer + css

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+/* Config file */
+module.exports = function(eleventyConfig) {
+  // Faire en sorte que dès qu'il y a du mouvement dans le dossier scss
+  // ça rebuild
+  eleventyConfig.addWatchTarget("./src/scss/");
+
+  // Ce dossier est cloné lors du build
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/css");
+
+  return { 
+    dir: {
+        input: "src",
+        output: "public"
+    }
+  }
+};
