75e5b0830b174c7007eedcb062555c091cde1b3f
soler marjorie <soler.marjorie81@gmail.com>
2022-10-07 10:35:27 +0200

    ajout des commentaires

--- src/js/galeries.js
+++ src/js/galeries.js
@@ -1,15 +1,18 @@
+
+//variables qui récupèrent les boutons et les galeries photos
 let myButtons = document.querySelectorAll('.visiblePhoto');
 let galeries = document.querySelectorAll('.photos');
 
+//fonction forEach qui dit que pour chaque boutons, l'action se déclanchera
 myButtons.forEach((btn, i) => {
+
+    //fonction qui rend visible ou non les galeries
     function galerie(){
         if (galeries[i].style.display === "none") {
             galeries[i].style.display = "block";
         } else {
             galeries[i].style.display = "none";
         }
-
-        return btn[i] = galeries[i];
-    };
+    }
     btn.addEventListener('click', galerie)
 });
